621ab46de5734ac89e194469cb86d0140d3e3a40
Fabio <fabio.h.abreu99@gmail.com>
2024-05-13 17:21:49 -0300

    vercel insights

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,11 +1,13 @@
 import Router  from '../../routes';
 import {LanguageProvider} from '../../contexts';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
     return (
     <LanguageProvider>
         <main className='container'>
             <Router />
+            <SpeedInsights />
         </main>
     </LanguageProvider>
     );
