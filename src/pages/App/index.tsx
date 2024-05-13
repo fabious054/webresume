4791c465b44b70cd94a7b4db0219b5eed9775c70
Fabio <fabio.h.abreu99@gmail.com>
2024-05-13 17:25:32 -0300

    vercel analytics

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,6 +1,7 @@
 import Router  from '../../routes';
 import {LanguageProvider} from '../../contexts';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
         <main className='container'>
             <Router />
             <SpeedInsights />
+            <Analytics />
         </main>
     </LanguageProvider>
     );
