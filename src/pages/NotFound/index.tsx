21f803e4aac0705b1bb6bd332e717ee4f55ec4e6
Fabio <fabio.h.abreu99@gmail.com>
2024-05-19 00:49:11 -0300

    page not found style | default variables

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,6 +2,7 @@ import styles from './NotFound.module.scss';
 import { LanguageContext } from '../../contexts';
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ButtonSetLanguage from '../../components/ButtonSetLanguage';
 
 export default function NotFound() {
     const navigate = useNavigate();
@@ -13,9 +14,10 @@ export default function NotFound() {
 
     return (
         <div className={styles.container}>
+            <ButtonSetLanguage />
             <h1>404</h1>
             <h2>{txt}</h2>
-            <button onClick={() => navigate(-1)}>{btnTxt}</button>
+            <button onClick={() => navigate('/')}>{btnTxt}</button>
         </div>
     );
 }
