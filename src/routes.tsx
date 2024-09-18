62dcf904a54f80c4b640c8effd7c5c99f9c93ab3
Fabio <fabio.h.abreu99@gmail.com>
2024-09-18 18:26:37 -0300

    some changes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,9 +3,6 @@ import { useLocation } from 'react-router-dom';
 
 
 import { Home } from './pages/home';
-import Aside from './components/Aside';
-import Footer from './components/Footer';
-import ButtonSetLanguage from './components/ButtonSetLanguage';
 import ContentPage from './pages/contentPage';
 import NotFound from './pages/NotFound';
 import Certificate from './pages/Certificate';
